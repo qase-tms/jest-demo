a32ad75657fee33cadadc8a4f2ef85e2a20ff87d
Manju R <cskmnrpt@gmail.com>
2024-12-22 19:49:42 +0530

    fix: id.test.js

--- test/examples/id.test.js
+++ test/examples/id.test.js
@@ -3,7 +3,7 @@ const { qase } = require("jest-qase-reporter/jest");
 describe("Example: id.spec.js", () => {
   /* Delete this line, and 
   // Please, change the Id from `1` to any case Id present in your project before uncommenting the test.
-  test(qase(6243, "Defining Id: Format 1"), () => {
+  test(qase(1, "A test with Qase Id"), () => {
     expect(true).toBe(true);
   });
   */

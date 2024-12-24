0d3673186e568fa1da3a888c18fa749e2a4f86bd
Karan710 <karanpuri454@gmail.com>
2024-12-24 15:19:09 +0530

    Revert "first commit"
    This reverts commit fbaf3e5d7984a1fb58e3a4325fe7589dce25c95a.

--- jest.config.js
+++ jest.config.js
@@ -4,25 +4,25 @@ module.exports = {
     [
       "jest-qase-reporter",
       {
-
+        /*
         // You can define the reporter options here, or in a separate `qase.config.json` file.
         mode: 'testops',
         debug: false,
         testops: {
           api: {
-            token: '',
+            token: 'api_key',
           },
-          project: '',
+          project: 'project_code',
           uploadAttachments: true,
           run: {
           //  id: 1,
-            title: "Test Test run from Jest ",
+            title: "Your test run title",
             description: "Automated Test run by Jest",
             complete: true,
           },
           environment: 'prod',
         },
-
+      */
       },
     ],
   ],

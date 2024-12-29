504d8a91571a0c80dbfe81d9a64d0e5a4b81f862
Manju R <cskmnrpt@gmail.com>
2024-12-29 19:24:10 +0530

    remove: chain.test.js

--- test/examples/ignore.test.js
+++ test/examples/ignore.test.js
@@ -1,7 +1,7 @@
 const { qase } = require("jest-qase-reporter/jest");
 
 describe("Example: ignore.test.js", () => {
-  test("This test is executed using Playwright; however, it is NOT reported to Qase", () => {
+  test("This test is executed using Jest; however, it is NOT reported to Qase", () => {
     qase.ignore();
     expect(true).toBe(true);
   });
